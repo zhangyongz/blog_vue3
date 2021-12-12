176d8d25fe41a786ac82e2fb49228b2d53d68f72
zhangyong <zhangyong_web@163.com>
2021-12-12 19:19:01 +0800

    feat: add publicPath and so on

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,12 @@
 /* eslint-disable */
 const Components = require('unplugin-vue-components/webpack');
 const { ElementPlusResolver } = require('unplugin-vue-components/resolvers');
+const isDevelopment = process.env.VUE_APP_ENV === 'development'
 
 module.exports = {
+  publicPath: isDevelopment ? '/' : './',
+  assetsDir: 'static',
+  productionSourceMap: false,
   configureWebpack: {
     plugins: [
       Components({
